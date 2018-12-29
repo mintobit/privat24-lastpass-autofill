c4e338be3a86b68f5444c887bca81ef1e999fe98
Anton Nizhegorodov <mintobit@gmail.com>
2018-12-30 01:04:58 +0200

    Implement name attribute injection

---
+++ privat24-lastpass-autofill.user.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name        Privat24 LastPass autofill
+// @version     1.0.0
+// @description LastPass can not autofill inputs that don't have "name" attribute. This script adds name="lastpass-p24-phone-numer" to phone element
+// @license     MIT
+// @author      Anton Nizhegorodov
+// @namespace   https://github.com/mintobit
+// @updateURL   https://raw.githubusercontent.com/mintobit/privat24-lastpass-autofill/master/privat24-lastpass-autofill.user.js
+// @downloadURL https://raw.githubusercontent.com/mintobit/privat24-lastpass-autofill/master/privat24-lastpass-autofill.user.js
+// ==/UserScript==
+
+(function(document) {
+    'use strict';
+
+    function injectInputName()
+    {
+        var phoneInput = document.querySelector('.module-login input[type=text][data-qa-node=login-number]');
+        if (null !== phoneInput && phoneInput.name === "") {
+            phoneInput.name = 'lastpass-p24-phone-number';
+            clearInterval(intervalId);
+        }
+    }
+
+    var intervalId = setInterval(injectInputName, 200);
+})(document);
